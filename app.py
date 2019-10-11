def7b84354976a7c36ef30556833310fbf91a41e
Yafei Guo <yafei-guo@uiowa.edu>
2019-10-10 20:22:42 -0500

    modefied application to use basic jinjatemplates

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='My Calculator')
 
 @app.route('/yafei')
 def yafei():
-    return "hello yafei"
+    return render_template('yafei.html', pageTitle='About Yafei')
 
 if __name__ == '__main__':
     app.run(debug=True)
