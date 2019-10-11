a741c9d7c58bfbd03a8506117064ff5961506ea4
Yafei Guo <yafei-guo@uiowa.edu>
2019-10-10 22:47:38 -0500

    completed calculate function

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='My Calculator')
+    return render_template('index.html', display="", pageTitle='Loan Calculator')
 
-@app.route('/yafei')
-def yafei():
-    return render_template('yafei.html', pageTitle='About Yafei')
+@app.route('/divide', methods=['GET', 'POST'])
+def divide():
+    if request.method =='POST':
+        form=request.form
+        loan = float(form['numOne'])
+        numPay = float(form['numTwo'])
+        year = float(form['numThree'])
+        rate = float(form['numFour'])
+        period = float(form['numFive'])
+        n=numPay*year
+        i=rate/period
+        a= 1+ i
+        b=a**n
+        c = b-1
+        d = c/(i *b)
+        calc = loan/d
+        return render_template('index.html', display=calc, pageTitle='My Calculator')
+
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
